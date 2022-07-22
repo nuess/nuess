f89d9d481143db92624503f14ebfd6f1a0bdb394
Andres Celis <adwher@icloud.com>
2022-07-21 21:07:19 -0500

    break(templating): remove `templating` package

--- packages/templating/types/utils.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Represents an optional value which can be {@link T} or `undefined`
- * @example
- * ```ts
- * const text0: Option<string> = "hello" // valid
- * const text1: Option<string> = undefined // also, is valid
- * ```
- */
-declare type Maybe<T> = T | undefined
